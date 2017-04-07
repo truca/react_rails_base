0ce35bd532e6f5762cd1f7e82d8eff6cf4873628
Ignacio Ureta <ignacio.ureta@live.com>
2017-04-07 12:15:06 -0300

    ended repos fetcher

--- client/app/bundles/HelloWorld/components/HelloWorld.jsx
+++ client/app/bundles/HelloWorld/components/HelloWorld.jsx
@@ -1,6 +1,45 @@
 import React, { PropTypes } from 'react';
+import axios from 'axios';
 
-export default class HelloWorld extends React.Component {
+const HelloWorld = React.createClass({
+  getInitialState(){
+    return { name: this.props.name, repositories: [] };
+  },
+  updateName(name){
+    this.setState({ name });
+    axios.get('https://api.github.com/users/' + name + '/repos')
+    .then(res => {
+      console.log('success', res)
+      this.setState({repositories: res.data})
+    })
+    .catch(err => {
+      console.log('err', err);
+    })
+  },
+  render(){
+    return (
+      <div>
+        <h3> Hello, {this.state.name}! </h3>
+        <hr />
+        <form >
+          <label htmlFor="name"> Say hello to:</label>
+          <input id="name" type="text" value={this.state.name} onChange={(e) => this.updateName(e.target.value)} />
+        </form>
+        <div>
+          <h3>Repositorios</h3>
+          <ul>
+            {this.state.repositories.map(repo => {
+              return <li>{repo.name}</li>
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+})
+
+export default HelloWorld;
+/*export default class HelloWorld extends React.Component {
   static propTypes = {
     name: PropTypes.string.isRequired, // this is passed from the Rails view
   };
@@ -8,7 +47,7 @@ export default class HelloWorld extends React.Component {
   /**
    * @param props - Comes from your rails view.
    * @param _railsContext - Comes from React on Rails
-   */
+   * /
   constructor(props, _railsContext) {
     super(props);
 
@@ -16,30 +55,19 @@ export default class HelloWorld extends React.Component {
     // https://facebook.github.io/react/docs/reusable-components.html#es6-classes
     this.state = { name: this.props.name };
   }
-
   updateName = (name) => {
     this.setState({ name });
   };
-
   render() {
     return (
       <div>
-        <h3>
-          Hello, {this.state.name}!
-        </h3>
+        <h3> Hello, {this.state.name}! </h3>
         <hr />
         <form >
-          <label htmlFor="name">
-            Say hello to:
-          </label>
-          <input
-            id="name"
-            type="text"
-            value={this.state.name}
-            onChange={(e) => this.updateName(e.target.value)}
-          />
+          <label htmlFor="name"> Say hello to:</label>
+          <input id="name" type="text" value={this.state.name} onChange={(e) => this.updateName(e.target.value)} />
         </form>
       </div>
     );
   }
-}
+}*/
